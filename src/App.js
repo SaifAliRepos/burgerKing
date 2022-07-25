a42c98e8770f9a1bdb4f72a26c9906026a220cfc
SaifAlidevs <saif.ali@devsinc.com>
2022-07-25 19:53:13 +0500

    Code cleaning

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
 import './App.css';
-import { Row, Col, Container } from 'react-bootstrap';
+
 import NavScrollExample from './components/Header';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Button } from 'bootstrap';
-import Footer from './components/Footer';
 import Burger from './components/Burger';
-import { NameForm } from './components/Testing';
-import { Example } from './components/use';
 
 function App() {
   return (
