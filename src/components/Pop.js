27a4c21260c2f915df60f4292a49e0c79379197e
SaifAlidevs <saif.ali@devsinc.com>
2022-07-25 20:18:29 +0500

    Commit for creating PR

--- src/components/Pop.js
+++ src/components/Pop.js
@@ -4,7 +4,6 @@ import Modal from 'react-bootstrap/Modal';
 
 export default function Pop() {
   const [show, setShow] = useState(false);
-
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
